008b6df7a53ae7b14e7635024604d02842e33e9e
Zewen Wang <w.zewen@outlook.com>
2018-01-26 09:48:53 -0500

    Polish the UI, cannot use Twitter service to share because SLServiceTypeTwitter is deprecated

--- Hotdog/Hotdog/ViewController.swift
+++ Hotdog/Hotdog/ViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 import VisualRecognitionV3
-
+import SVProgressHUD
+import Social
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     let apiKey = "711fbc31b523ebc2e73cf41e162251688da3c1ed"
     let version = "2018-01-25"
     var classificationResults : [String] = []
+    
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var topBarImageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var imageView: UIImageView!
     let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
+        shareButton.isHidden = true
         imagePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -26,7 +31,14 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // disable the camera btn and enable the spinner
+        cameraButton.isEnabled = false
+        SVProgressHUD.show()
+        
+        
+        
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
             imageView.image = image
             imagePicker.dismiss(animated: true, completion: nil)
@@ -42,26 +54,51 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
                 for index in 0..<classes.count{
                     self.classificationResults.append(classes[index].classification)
                 }
+                
                 print(self.classificationResults)
+                DispatchQueue.main.async {
+                    self.cameraButton.isEnabled = true
+                    SVProgressHUD.dismiss()
+                }
                 if self.classificationResults.contains("hotdog"){
                     DispatchQueue.main.async {
                         self.navigationItem.title = "Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        self.topBarImageView.image = UIImage(named: "hotdog")
+                        self.shareButton.isHidden = false
                     }
                 }
                 else{
                     DispatchQueue.main.async {
                         self.navigationItem.title = "Not Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.red
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        self.topBarImageView.image = UIImage(named: "not-hotdog")
+                        self.shareButton.isHidden = false
+
                     }
                 }
             })
             
-        }else{
+        }
+        
+        else{
             print("There was an error")
         }
     }
-
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter){
+            let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            vc?.setInitialText("My food is \(navigationItem.title)")
+            vc?.add(#imageLiteral(resourceName: "hotdogBackground"))
+            present(vc!, animated: true, completion: nil)
+        }else{
+            self.navigationItem.title = "Please login your Twitter"
+        }
+    }
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
-        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.sourceType = .savedPhotosAlbum // could use .savedPhotosAlbum for testing
         imagePicker.allowsEditing = false
         present(imagePicker, animated: true, completion: nil)
     }
